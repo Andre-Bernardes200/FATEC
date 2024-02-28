2bd07adcaece74f8da9f857fa22de533cb444a11
Andre Bernardes <andreluiz1088@hotmail.com>
2024-02-27 22:46:19 -0300

    Criação do baralho arrumado

--- LP3/src/baralho/models/Baralho.java
+++ LP3/src/baralho/models/Baralho.java
@@ -13,11 +13,10 @@ public class Baralho {
         for(int i = 0; i < 14; i++){
             for(int n = 0; n < 5; n++){
                 if(listaNomes[i] != Nome.CORINGA && listaNaipes[n] != Naipe.CORINGA){
-                    Carta carta = new Carta(listaNomes[n],listaNaipes[i]);
+                    Carta carta = new Carta(listaNomes[i],listaNaipes[n]);
                     cartas.add(carta);
                 }else if (listaNomes[i] == Nome.CORINGA && listaNaipes[n] == Naipe.CORINGA) {
                     Carta carta = new Carta(listaNomes[i],listaNaipes[n]);
-
                     cartas.add(carta);
                 }
             }
