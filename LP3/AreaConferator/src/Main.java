a3e96deba973fa214ae52d866db6293f303a2dd1
Andre-Bernardes200 <andreluiz1088@hotmail.com>
2024-06-04 19:36:23 -0300

    instancia

--- LP3/AreaConferator/src/Main.java
+++ LP3/AreaConferator/src/Main.java
@@ -1,5 +1,7 @@
+import enums.Figuras;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Factory.factory(Figuras.QUADRADO,20);
     }
 }
